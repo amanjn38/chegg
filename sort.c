9b84965385072276b3e061d411f546e25b46dfe1
vaibhav sachan <46049646+butowskii@users.noreply.github.com>
2020-06-30 02:11:54 +0530

    Update sort.c

--- sort.c
+++ sort.c
@@ -1,7 +1,6 @@
 sort(point a[],int len,int t){
-    float arr[len];
-    for(int i=0;i<len;i++){
-        arr[i]=sqrt((a[i].x-X)*(a[i].x-X)+(a[i].y-Y)*(a[i].y-Y));
-    }
-    mergeSort(arr,0,len-1,t);
+    if(t>len)
+    mergeSort(a,0,len-1,t);
+    else 
+        insertionSort(a,0,len-1,t);
 }
